2d4b426bf96973c0e64d48f497e12ce2ede80016
wangdh <wangdh@gzsendi.cn>
2020-04-29 13:46:32 +0800

    新增项目

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id='header'>
+        {{message}}
+    </div>
+</template>
+
+<script>
+  export default {
+    name:'#header',
+    data() {
+        return {
+            message:'测试'
+        }     
+    },
+    
+  }
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <!-- <h1>{{ msg }}</h1>  -->
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from './Header';
+export default {
+  name: 'HelloWorld',
+  // data () {
+  //   return {
+  //     msg: '第一个练习'
+  //   }
+  // }
+  components:{
+    Header,
+  }
+}
+</script>
+
+
